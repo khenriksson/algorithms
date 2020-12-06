8176b836e631c274fa9a4e85c5b46316bc8611a9
Kasper Henriksson <kasper.henriksson@aalto.fi>
2020-12-06 15:15:11 +0200

    Second done

--- adventOfCode2020/day6/main.py
+++ adventOfCode2020/day6/main.py
@@ -9,13 +9,26 @@ for line in f:
         arr.extend(line.split("\n")[0].split(" "))
 
     if not line.strip():
-        test = ''.join(set(arr))
+        # print(arr)
+        # print(len(arr))
+        print(arr)
+        test = ''.join(arr)
+        check = test.count("a")
+        # print(check,"    " ,      test)
+        # print(test)
         reduced_word = ''.join(
             [char for index, char in enumerate(test) if char not in test[0:index]])
-        count = len(reduced_word)
-        print(count)
-        print(reduced_word)
-        summed += count
+        for i in reduced_word:
+            print(i, "           ", test, "          ", reduced_word)
+            if (test.count(i) == len(arr)): 
+                # print(test.count(i))
+                print("In here")
+                summed += 1
+                print(summed)
+        # count = len(reduced_word)
+        # print(count)
+        # print(reduced_word)
+
         arr = []
         # print(test)
 
