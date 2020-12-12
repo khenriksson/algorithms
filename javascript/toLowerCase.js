9c9f79bc57a1e0aba9374ca042f7bcef9f333eb9
Kasper Henriksson <kasper.henriksson@aalto.fi>
2020-12-12 12:13:45 +0200

    To lowercase done

---
+++ javascript/toLowerCase.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function (str) {
+  return str.toLowerCase()
+}
+
+/**
+ * Runtime: 76 ms, faster than 67.77% of JavaScript online submissions for To Lower Case.
+ * Memory Usage: 38.5 MB, less than 15.98% of JavaScript online submissions for To Lower Case.
+ */
