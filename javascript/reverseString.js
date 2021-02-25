aa13aa5429e39a27d88f242e475d682b5ab28bf7
Kasper Henriksson <kasper.henriksson@aalto.fi>
2021-02-25 23:14:38 +0200

    reverse string done

---
+++ javascript/reverseString.js
@@ -0,0 +1,12 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  return s.reverse()
+}
+
+/**
+ * Runtime: 92 ms, faster than 99.71% of JavaScript online submissions for Reverse String.
+ * Memory Usage: 45.5 MB, less than 89.75% of JavaScript online submissions for Reverse String.
+ */
